a7fee7ee6b8484b4dc8163bfaf1c90519d277b99
vietman2 <vietman0@gmail.com>
2023-08-30 20:48:13 +0900

    add user type

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,35 +1,11 @@
 import { Provider as ReduxProvider } from "react-redux";
 
-import Base from "./components/Account/Base";
-//import MainContainer from "./containers/MainContainer";
-import { AuthProvider } from "./components/Account/AuthProvider/AuthProvider";
 import store from "./store/store";
+import RootContainer from "./containers/RootContainer";
 
-function App(){
-  /*
-  useEffect(() => {
-    const checkToken = async () => {
-      const token = AsyncStorage.getItem("token");
-      if(token) {// Check if token is valid
-        // If valid, navigate to main container
-        return (
-          <MainContainer/>
-        )
-        // If not valid, navigate to login
-        return (
-          <Login/>
-        )
-    }
-    */
-
-  // 아래는 Temporary
-  return (
-    <AuthProvider>
-      <ReduxProvider store={store}>
-        {/* <MainContainer /> */}
-        <Base />
-      </ReduxProvider>
-    </AuthProvider>
-  );
+function App() {
+  return <ReduxProvider store={store}>
+    <RootContainer />
+  </ReduxProvider>;
 }
 export default App;
