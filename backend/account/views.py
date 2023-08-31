4c3444727de7804615231e31810b5ab4c7767774
vietman2 <vietman0@gmail.com>
2023-08-31 21:54:48 +0900

    lint

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,10 +1,11 @@
-from .models import UserProfile,CustomUser
-from .serializers import UserSerializer,UserProfileSerializer
-
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import UserProfile,CustomUser
+from .serializers import UserSerializer,UserProfileSerializer
+
 
 def set_token_on_response_cookie(user: CustomUser) -> Response:
     token = RefreshToken.for_user(user)
--- backend/baseball_team/settings.py
+++ backend/baseball_team/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 from environ import Env
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
