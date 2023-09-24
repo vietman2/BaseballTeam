a41cdaaaef7551b26cab280dc79ffcd7277dfe2e
vietman2 <vietman0@gmail.com>
2023-09-24 16:10:59 +0900

    unnecessary whitespace

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -2,7 +2,6 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import ValidationError
 from .models import CustomUser
 
-
 class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
