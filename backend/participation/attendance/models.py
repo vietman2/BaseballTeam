35f7551510d9a15a624ad706faf18364d1236d68
vietman2 <vietman0@gmail.com>
2023-10-04 01:07:28 +0900

    comment out models for now

--- backend/participation/attendance/models.py
+++ backend/participation/attendance/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from account.models import CustomUser
-from session.models import Session
 
-# Create your models here.
-class Attendance:
-  user = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
-  session = models.ForeignKey("Session", on_delete=models.CASCADE)
-  start_time = models.TimeField()
-  end_time = models.TimeField()
+#class Attendance(models.Model):
+  #user = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+  #session = models.ForeignKey("Session", on_delete=models.CASCADE)
+  #start_time = models.TimeField()
+  #end_time = models.TimeField()
--- backend/participation/session/models.py
+++ backend/participation/session/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-from attendance.models import Attendance
-from account.models import CustomUser
 
-# Create your models here.
-class Session(models.Model):
-    date = models.DateField()
-    day = models.CharField(max_length=10)
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    attendance_users = models.ManyToManyField("CustomUser",related_name='attendance_sessions', through='Attendance')
+#class Session(models.Model):
+#    date = models.DateField()
+#    day = models.CharField(max_length=10)
+#    start_time = models.TimeField()
+#    end_time = models.TimeField()
+#    attendance_users = models.ManyToManyField("CustomUser",related_name='attendance_sessions', through='Attendance')
 
-class TrainingType(models.Model):
-    type = models.CharField(max_length=50)
+#class TrainingType(models.Model):
+#    type = models.CharField(max_length=50)
