8674033b81f46f20d186c3f67dcdee54a80f9ef3
vietman2 <vietman0@gmail.com>
2023-10-04 01:06:47 +0900

    fix app name for correct application

--- backend/participation/session/apps.py
+++ backend/participation/session/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SessionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'session'
+    name = 'participation.session'
--- backend/participation/week/apps.py
+++ backend/participation/week/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WeekConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'week'
+    name = 'participation.week'
