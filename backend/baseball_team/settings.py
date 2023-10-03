67db82e365d0f9bcf7c48ef43e6d1fab5ff92a1a
vietman2 <vietman0@gmail.com>
2023-10-04 01:14:13 +0900

    fix import order

--- backend/baseball_team/settings.py
+++ backend/baseball_team/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
