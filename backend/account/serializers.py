bb448ca6a6b1fad2f0fb236e8f98103db744bdec
vietman2 <vietman0@gmail.com>
2023-10-03 22:13:06 +0900

    divide serializers by role

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -2,25 +2,45 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import ValidationError
 from .models import CustomUser
 
-class UserSerializer(ModelSerializer):
+class UserRegisterSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["username", "password", "phone_number"]
+        fields = ["user_type", "name", "phone_number", "major", "grade", "position", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
-    def validate(self, attrs):
-        username = attrs.get('username', '')
-        password = attrs.get('password', '')
-        phone_number = attrs.get('phone_number','')
-        if not (username and password and phone_number):
-            raise ValidationError({"detail": "[password, username, phone_number] fields missing."})
-        return attrs
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+    
+    def validate(self, data):
+        if data["user_type"] == 1:
+            raise ValidationError("관리자는 직접 생성할 수 없습니다")
+        ## 클라이언트에서 전화번호가 - 없이 010xxxxyyyy 형태로 들어오면
+        ## 여기서 -를 붙여주도록 구현
+        phone_number = data["phone_number"]
+        if phone_number.find("-") == -1:
+            data["phone_number"] = phone_number[:3] + "-" + phone_number[3:7] + "-" + phone_number[7:]
+
+        return data
+    
+class UserLoginSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["phone_number", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+class UserPasswordChangeSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
 class UserAuthorityCheckSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["role"]
+        fields = ["user_type"]
 
 class UserProfileSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["is_pitcher", "is_active", "position", "grade", "major", "name"]
+        fields = ["name", "phone_number", "major", "grade", "position"]
