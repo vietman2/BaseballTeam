f331fccd506ad23b0d3a99ee95db0eb16e0d1d23
vietman2 <vietman0@gmail.com>
2023-10-06 09:12:52 +0900

    remove unnecessary variable

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import RegisterView, UserProfileView
 
-app_name = 'account'
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     # path()
