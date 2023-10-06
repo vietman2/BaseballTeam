32aaa96c4bdddb8333ff85c5401afa03e28d327d
vietman2 <vietman0@gmail.com>
2023-10-06 09:39:26 +0900

    session viewset

--- backend/participation/session/views.py
+++ backend/participation/session/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Session
+
+class SessionView(ModelViewSet):
+    queryset = Session.objects.all()
+    #permission_classes = [IsAuthenticated]
+    #serializer_class = SessionSerializer
