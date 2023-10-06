cf99351724da1cf6efab033125deca2138b06665
vietman2 <vietman0@gmail.com>
2023-10-06 09:39:09 +0900

    initial model setup

--- backend/participation/session/models.py
+++ backend/participation/session/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
-#class Session(models.Model):
-#    date = models.DateField()
-#    day = models.CharField(max_length=10)
-#    start_time = models.TimeField()
-#    end_time = models.TimeField()
-#    attendance_users = models.ManyToManyField("CustomUser",related_name='attendance_sessions', through='Attendance')
+class Session(models.Model):
+    class DayChoices(models.TextChoices):
+        MONDAY = 1, "월요일"
+        TUESDAY = 2, "화요일"
+        WEDNESDAY = 3, "수요일"
+        THURSDAY = 4, "목요일"
+        FRIDAY = 5, "금요일"
+        SATURDAY = 6, "토요일"
+        SUNDAY = 7, "일요일"
 
-#class TrainingType(models.Model):
-#    type = models.CharField(max_length=50)
+    date = models.DateField()
+    day = models.IntegerChoices(choices=DayChoices.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    training_type = models.ForeignKey("TrainingType", on_delete=models.SET_NULL, related_name='sessions')
+
+    class Meta:
+        ordering = ['date', 'start_time']
+        db_table = 'session'
+        verbose_name = 'session'
+        verbose_name_plural = 'sessions'
+        db_table_comment = "세션 = 훈련 한 타임"
+
+class TrainingType(models.Model):
+    type = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
