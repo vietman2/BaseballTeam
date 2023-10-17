7069ba3c3f60a6514fbd3d62ba04536d6ba09166
daeguhighman <keroro19671967@gmail.com>
2023-10-17 23:04:34 +0900

    logout, signup api 작성

--- frontend/services/account.ts
+++ frontend/services/account.ts
@@ -13,7 +13,7 @@ export async function login(email: string, password: string) {
 
         await AsyncStorage.setItem("accessToken", response.data.access_token);
         await AsyncStorage.setItem("refreshToken", response.data.refresh_token);
-
+        console.log(response.data);
         return response.data;
     } catch (error: any) {
         return error.response.data;
@@ -22,9 +22,37 @@ export async function login(email: string, password: string) {
 
 export async function logout() {
     // TODO: 로그아웃 API 호출, 토큰 삭제
+    const url = `${API_BASE_URL}/api/account/signout/`;
+    try{
+        const response = await axios.post(url, {
+            headers: {
+                Authorization: `Bearer ${await AsyncStorage.getItem("accessToken")}`,
+            },
+        });
+        await AsyncStorage.removeItem("accessToken");
+        await AsyncStorage.removeItem("refreshToken");
+        console.log(response.data);
+        return response.data;
+    } catch (error: any) {
+        return error.response.data;
+    }
 }
 
-export async function signup() {
+export async function signup (user_type: number, name: string, phone_number: string, major: string, grade: number, position: number, password: string) {
     // TODO: 회원가입 API 호출
-}
+    const url = `${API_BASE_URL}/api/account/signup/`;
+    try{
+        const response = await axios.post(url, {
+            user_type, name, phone_number, major, grade, position, password
+        });
+        await AsyncStorage.setItem("accessToken", response.data.access_token);
+        await AsyncStorage.setItem("refreshToken", response.data.refresh_token);
+        console.log(response.data);
+        return response.data;
+    } catch (error: any) {
+        return error.response.data;
+    }
+    }
+
+
 
