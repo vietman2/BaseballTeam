e9c6db90417c2693deecbafd447ae17f61818ffd
vietman2 <vietman0@gmail.com>
2023-12-20 18:37:34 +0900

    remove unnecessary test function

--- frontend/src/components/WeeklyTable/WeeklyTable.test.tsx
+++ frontend/src/components/WeeklyTable/WeeklyTable.test.tsx
@@ -6,8 +6,4 @@ describe("<WeeklyTable />", () => {
   it("should render correctly", () => {
     render(<WeeklyTable />);
   });
-
-  it("should return data correctly", () => {
-    const { getByText } = render(<WeeklyTable />);
-  });
 });
